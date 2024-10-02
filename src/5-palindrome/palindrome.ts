02010a2f8d76ebeae3b934489d8b8f007e0c9292
VictorHMDias <victor.henrique14@gmail.com>
2024-10-01 23:55:03 -0300

    Exercicio_3

---
+++ src/5-palindrome/palindrome.ts
@@ -0,0 +1,6 @@
+// palindrome.ts
+export const isPalindrome = (str: string): boolean => {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedStr = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedStr;
+};
